2c93757086cddee641f215f82158213074223dcd
Student <Student@10.5.4.9>
2013-10-16 13:10:44 +0200

    Hello!

---
+++ GitHubTest/src/githubtest/GitHubTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author Student
+ */
+public class GitHubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+    }
+}
